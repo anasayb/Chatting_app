4705f046511c50b1256c8199c103ac91d27085f7
anasayb <anasayb@hotmail.com>
2022-06-18 00:08:28 +0300

    Added name feature, allow the user to send multiple messages, allow the user to diconnect whenever

--- Client.py
+++ Client.py
@@ -1,6 +1,4 @@
-# TODO: take input form the user then send
-# TODO: allow the user to decide when to stop the connection
-
+# TODO: make the program only ask the user for the name in the first time running the program
 
 import socket
 
@@ -29,5 +27,14 @@ def send(message):
     client.send(length_encoded)
     client.send(msg_encoded)
 
-send("Hello world")
+# This method is used to inizilize the information between client and server after creating the connection
+def inizilize():
+    name = input("Please choose a name: ")
+    send(name)
+
+inizilize()
+msg = ""
+while msg != DISCONNECT:
+    msg = input(f"[ YOU ] ")
+    send(msg)
 send(DISCONNECT)
